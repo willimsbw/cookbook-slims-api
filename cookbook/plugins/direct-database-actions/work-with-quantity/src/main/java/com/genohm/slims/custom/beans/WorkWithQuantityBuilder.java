b3375a4fe3b7d15cef122906cd288eb5a516fe0c
stefan <stefan.froelicher@agilent.com>
2025-01-07 10:56:30 +0100

    SLIMSSVC-12555: change .to by .bean to be more consistent

--- cookbook/plugins/direct-database-actions/work-with-quantity/src/main/java/com/genohm/slims/custom/beans/WorkWithQuantityBuilder.java
+++ cookbook/plugins/direct-database-actions/work-with-quantity/src/main/java/com/genohm/slims/custom/beans/WorkWithQuantityBuilder.java
@@ -5,15 +5,18 @@
 package com.genohm.slims.custom.beans;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WorkWithQuantityBuilder extends RouteBuilder {
+	@Autowired
+	private WorkWithQuantityBean workWithQuantityBean;
 
 	@Override
 	public void configure() throws Exception {
 		from("direct:work-with-quantity")
-			.to("bean:workWithQuantityBean")
+			.bean(workWithQuantityBean)
 			.routeId("work-with-quantity");
 	}
 
