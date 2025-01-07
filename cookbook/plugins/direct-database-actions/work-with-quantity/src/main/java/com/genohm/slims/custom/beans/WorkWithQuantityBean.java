92039d0f506837a59c3c1cadf5137efefaaefc3c
stefan <stefan.froelicher@agilent.com>
2025-01-07 10:51:50 +0100

    SLIMSSVC-12555: add or fix comments + refactor based on review comments

--- cookbook/plugins/direct-database-actions/work-with-quantity/src/main/java/com/genohm/slims/custom/beans/WorkWithQuantityBean.java
+++ cookbook/plugins/direct-database-actions/work-with-quantity/src/main/java/com/genohm/slims/custom/beans/WorkWithQuantityBean.java
@@ -48,19 +48,23 @@ public class WorkWithQuantityBean {
 		Quantity initialVolume = quantityService.castOrConvertToQuantity(content.get("cntn_cf_volume"));
 		LOGGER.info("After the 'cast or convert' we have this value {} and the type is {}. This will allow us to perform actions on this Quantity.", initialVolume, initialVolume.getClass().getName());
 
-		// Create 2 new Quantities (either from a map or from a Quantity directly)
-		Quantity volumeToSubstract = new Quantity(2, unitService.convertToUnit(workWithQuantityConfiguration.getQuantityToSubtractUnitName()));
+		// Create 1 new Quantity Object
+		Quantity volumeToSubtract = new Quantity(2, unitService.convertToUnit(workWithQuantityConfiguration.getQuantityToSubtractUnitName()));
 
-		if (initialVolume.compareTo(volumeToSubstract) < 0){
+		// method `compareTo` is handling the unit conversion to do a correct comparison and will return:
+		//  -1  if `volumeToSubtract` is bigger than `initialVolume`
+		//  0   if `volumeToSubtract` is equal to `initialVolume`
+		//  1   if `volumeToSubtract` is smaller than `initialVolume`
+		if (initialVolume.compareTo(volumeToSubtract) < 0){
 			// Then we should not reduce the volume -> we will be in negative volume and this is not good
 			throw new SlimsGateErrorException(String.format("Error - Not enough volume. Initial volume is %s and we want to remove %s of it. This will lead to a negative volume, this is not accepted.",
-					initialVolume, volumeToSubstract));
+					initialVolume, volumeToSubtract));
 		}
 
-		// Substract 2 dl to the volume that is in ml
-		Quantity reducedVolume = initialVolume.substract(volumeToSubstract);
+		// Subtract 2 of the unit provided in the plugin configuration (default = `dl`) to the volume (unit is the one coming from the SLIMS field, for example `ml`)
+		Quantity reducedVolume = initialVolume.substract(volumeToSubtract);
 
-		// Convert to a certain unit
+		// Convert to a certain unit provided in the plugin configuration (parameter: `finalUnitName`)
 		Quantity finalVolume = reducedVolume.convertTo(unitService.convertToUnit(workWithQuantityConfiguration.getFinalUnitName()));
 
 		// update the record, another field with this new value
